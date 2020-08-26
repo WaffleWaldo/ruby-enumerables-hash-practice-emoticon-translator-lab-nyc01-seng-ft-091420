8e162479623ccc0dc675600452e11c2ecceede68
Oswaldo Ortiz <oswaldoortiz0519@gmail.com>
2020-08-26 17:08:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -25,7 +25,7 @@ def get_english_meaning(file_path, emoticon)
   
     hash.each do |key, value|
     if value.has_value?(emoticon)
-      return hash[key.to_s]
+      return hash[key]
     end
   end
   return 'Sorry, that emoticon was not found'
