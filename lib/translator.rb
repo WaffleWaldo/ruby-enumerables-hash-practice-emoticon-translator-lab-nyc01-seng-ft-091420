f18fa88db77ba636e75c3352704486b1a3f407a3
Oswaldo Ortiz <oswaldoortiz0519@gmail.com>
2020-08-26 17:04:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,7 @@ def get_japanese_emoticon(file_path, emoticon)
   
   hash.each do |key, value|
     if value.has_value?(emoticon)
-      return value[:english]
+      return value[:japanese]
     end
   end
   return 'Sorry, that emoticon was not found'
