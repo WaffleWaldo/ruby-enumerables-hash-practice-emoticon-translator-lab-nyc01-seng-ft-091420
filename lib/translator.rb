798dc8bbea07ff6e7a3b8174211e71c82484587b
Oswaldo Ortiz <oswaldoortiz0519@gmail.com>
2020-08-26 16:37:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,11 +10,11 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path, emoticon)
-  load_library(file_path)
+  hash = load_library(file_path)
   
   
 end
 
 def get_english_meaning(file_path, emoticon)
-  load_library(file_path)
+  hash = load_library(file_path)
 end
